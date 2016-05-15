083b0bcaf4cbcdab971b82a1cf472a0054c330c1
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-05-16 00:45:53 +0100

    Upgrade to netlist-graph 21f64a7

--- src/main/java/net/xprova/xprova/Main.java
+++ src/main/java/net/xprova/xprova/Main.java
@@ -1,32 +1,47 @@
 package net.xprova.xprova;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 
-import net.xprova.netlistgraph.Generator;
 import net.xprova.graph.Graph;
-import net.xprova.xprova.Manipulator;
-import net.xprova.xprova.Manipulator2;
 import net.xprova.netlist.GateLibrary;
 import net.xprova.netlist.Netlist;
 import net.xprova.netlistgraph.ConnectivityException;
+import net.xprova.netlistgraph.Generator;
 import net.xprova.netlistgraph.NetlistGraph;
 import net.xprova.netlistgraph.Vertex;
-import net.xprova.verilogparser.VerilogParser;
 import net.xprova.verilogparser.StructuralException;
 import net.xprova.verilogparser.UnsupportedGrammerException;
+import net.xprova.verilogparser.VerilogParser;
 
 public class Main {
 
-	private static void doCMDWork(String verilogFile, String outputVerilogFile) throws Exception {
+	private static GateLibrary loadLibrary(String libFile) throws Exception {
+
+		ArrayList<Netlist> libMods = VerilogParser.parseFile(libFile, new GateLibrary());
+
+		return new GateLibrary(libMods);
+
+	}
+
+	private static void doCMDWork(String verilogFile, String libFile, String outputVerilogFile) throws Exception {
 
 		System.out.println("input file  : " + verilogFile);
 
 		System.out.println("output file : " + outputVerilogFile);
 
-		GateLibrary library1 = new GateLibrary("faraday90nm");
+		GateLibrary library1 = loadLibrary(libFile);
 
-		Netlist netlist = VerilogParser.parse(verilogFile, library1);
+		ArrayList<Netlist> nlArr = VerilogParser.parseFile(verilogFile, library1);
+
+		Netlist netlist = nlArr.get(0);
+
+		if (nlArr.size() > 1) {
+
+			System.out.println("warning: input file contains multiple modules");
+
+		}
 
 		NetlistGraph graph = new NetlistGraph(netlist);
 
@@ -36,55 +51,57 @@ public class Main {
 
 		graph.printStats("Transformed");
 
-		Generator.generate(graph, outputVerilogFile);
+		Generator.generateFile(graph, outputVerilogFile);
 
 	}
 
-	private void doWork() throws Exception {
-
-		String verilogFile = "D:\\dev\\cdctool\\synthesized\\design_synth.v";
-
-		String dotFile = "D:\\dev\\cdctool\\synthesized\\graph.dot";
-
-		String outputVerilogFile = "D:\\dev\\cdctool\\synthesized\\design_synth_morphed.v";
-
-		GateLibrary library1 = new GateLibrary("faraday90nm");
-
-		Netlist netlist = VerilogParser.parse(verilogFile, library1);
-
-		NetlistGraph graph = new NetlistGraph(netlist);
-
-		// Manipulator.forkCDCPaths(graph);
-
-		// System.out.println(Manipulator.getHazardousFlops(graph,
-		// "clk2").toString());
-
-		graph.printStats("Original");
-
-		Manipulator2.transformCDC(graph);
-
-		graph.printStats("Transformed");
-
-		Vertex v = graph.getVertex("u2_reg1_q_reg_0_");
-
-		HashSet<Vertex> set1 = graph.bfs(v, Manipulator2.getFlops(graph), true);
-
-		set1.add(v);
-
-		graph.printGraph(dotFile);
-
-		// Manipulator.getMsFlopGraph(graph).printGraph(dotFile);
-
-		runCmd("dot -Tsvg D:\\dev\\cdctool\\synthesized\\graph.dot -o D:\\dev\\cdctool\\synthesized\\graph.svg");
-
-		// Manipulator.printMsFlopGraph(Manipulator.getMsFlopGraph(graph));
-
-		// produceFlopInputGraph(graph, graph.getVertex("u2_data_out_reg_0_"),
-		// dotFile);
-
-		Generator.generate(graph, outputVerilogFile);
-
-	}
+	// private void doWork() throws Exception {
+	//
+	// String verilogFile = "D:\\dev\\cdctool\\synthesized\\design_synth.v";
+	//
+	// String dotFile = "D:\\dev\\cdctool\\synthesized\\graph.dot";
+	//
+	// String outputVerilogFile =
+	// "D:\\dev\\cdctool\\synthesized\\design_synth_morphed.v";
+	//
+	// GateLibrary library1 = new GateLibrary("90nm");
+	//
+	// Netlist netlist = VerilogParser.parse(verilogFile, library1);
+	//
+	// NetlistGraph graph = new NetlistGraph(netlist);
+	//
+	// // Manipulator.forkCDCPaths(graph);
+	//
+	// // System.out.println(Manipulator.getHazardousFlops(graph,
+	// // "clk2").toString());
+	//
+	// graph.printStats("Original");
+	//
+	// Manipulator2.transformCDC(graph);
+	//
+	// graph.printStats("Transformed");
+	//
+	// Vertex v = graph.getVertex("u2_reg1_q_reg_0_");
+	//
+	// HashSet<Vertex> set1 = graph.bfs(v, Manipulator2.getFlops(graph), true);
+	//
+	// set1.add(v);
+	//
+	// graph.printGraph(dotFile);
+	//
+	// // Manipulator.getMsFlopGraph(graph).printGraph(dotFile);
+	//
+	// runCmd("dot -Tsvg D:\\dev\\cdctool\\synthesized\\graph.dot -o
+	// D:\\dev\\cdctool\\synthesized\\graph.svg");
+	//
+	// // Manipulator.printMsFlopGraph(Manipulator.getMsFlopGraph(graph));
+	//
+	// // produceFlopInputGraph(graph, graph.getVertex("u2_data_out_reg_0_"),
+	// // dotFile);
+	//
+	// Generator.generateFile(graph, outputVerilogFile);
+	//
+	// }
 
 	private static void runCmd(String cmd) throws IOException {
 
@@ -124,34 +141,30 @@ public class Main {
 		subgraph.printGraph(dotFile, ignoreList);
 
 	}
-	
+
 	public static void printUsage() {
-		
+
 		// following format guidelines from http://docopt.org/
-		
-		String[] usageMsgLines = {
-				"xprova v0.0.1-SNAPSHOT",
-				"",
-				"Usage:",
-				"  xprova source.v augmented.v",
-//				"",
-//				"Options:",
-//				"  -h --help  Show this screen.", // TODO
-//				"  --version  Show version.",		// TODO		
+
+		String[] usageMsgLines = { "xprova v0.0.1-SNAPSHOT", "", "Usage:", "  xprova source.v library.v augmented.v",
+				// "",
+				// "Options:",
+				// " -h --help Show this screen.", // TODO
+				// " --version Show version.", // TODO
 		};
-		
-		for (int i=0; i<usageMsgLines.length; i++)
+
+		for (int i = 0; i < usageMsgLines.length; i++)
 			System.out.println(usageMsgLines[i]);
-		
+
 	}
 
 	public static void main(String[] args) {
 
 		try {
 
-			if (args.length == 2) {
+			if (args.length == 3) {
 
-				doCMDWork(args[0], args[1]);
+				doCMDWork(args[0], args[1], args[2]);
 
 			} else {
 
